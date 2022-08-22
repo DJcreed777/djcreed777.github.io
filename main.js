b0bf40152fbcb16885782b5fb0e1cb4a549a0702
NDJohnsonJr <dexterjohnson620@gmail.com>
2022-08-21 17:38:20 -1000

    Added Javascript File

---
+++ main.js
@@ -0,0 +1,23 @@
+//Display Classes
+//Will display when user has clicked a class they like
+var Hero = "A stalwart Hero, at home with a battleaxe, descended from a badlands chieftain.";
+var Bandit = "A dangerous bandit who strikes for weak points. Excels at ranged combat with bows.";
+var Astrologer = "A scholar who reads fate in the stars. Heir to the school of glintstone sorcery.";
+var Warrior = "A twinblade wielding warrior from a nomadic tribe. An origin of exceptional technique.";
+var Prisoner = "A prisoner bound in an iron mask. Studied in glintstone sorcery, having lived among the elite prior to sentencing.";
+var Confessor = "A church spy adet at convert operations. Equally adept with a sword as they are with incantations.";
+var Wretch = "As poor, purposeless and naked as the day they were born. A nice club is all they have.";
+var Vagabond = "A knight exiled from their homeland to wander. A solid, armor-clad origin.";
+var Prophet = "A seer ostracized for inauspicious prophecies. Well versed in healing incantations.";
+var Samurai = "A capable fighter from the distant land of reeds. Handy with Katana and Longbows.";
+
+console.log(Hero)
+console.log(Bandit)
+console.log(Astrologer)
+console.log(Warrior)
+console.log(Prisoner)
+console.log(Confessor)
+console.log(Wretch)
+console.log(Vagabond)
+console.log(Prophet)
+console.log(Samurai)
