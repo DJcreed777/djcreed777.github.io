75813eae80b7e34e5b9b0d14202bb2418608f7e8
NDJohnsonJr <dexterjohnson620@gmail.com>
2022-08-28 11:57:23 -1000

    Added Javascript and User Input

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//User Input
+var nameBtn = document.getElementById("nameSetButton").onclick = nameSetApplication;
+
+function nameSetApplication()
+{
+    let nameSet = document.getElementById("Name Set");
+    var startHeading;
+    var endHeading;
+
+    startHeading = nameSet;
+    endHeading = nameSet;
+
+    document.getElementById("Start Heading").innerHTML = nameSet;
+    document.getElementById("End Heading").innerHTML = nameSet;
+    console.log(startHeading);
+    console.log(endHeading);
+}
+
 //Display Classes
 //Will display when user has clicked a class they like
 var Hero = "A stalwart Hero, at home with a battleaxe, descended from a badlands chieftain.";
@@ -11,13 +29,76 @@ var Vagabond = "A knight exiled from their homeland to wander. A solid, armor-cl
 var Prophet = "A seer ostracized for inauspicious prophecies. Well versed in healing incantations.";
 var Samurai = "A capable fighter from the distant land of reeds. Handy with Katana and Longbows.";
 
-console.log(Hero)
-console.log(Bandit)
-console.log(Astrologer)
-console.log(Warrior)
-console.log(Prisoner)
-console.log(Confessor)
-console.log(Wretch)
-console.log(Vagabond)
-console.log(Prophet)
-console.log(Samurai)
+var classBtn = document.getElementById("heroClassButton").onclick = displayClass1;
+var classBtn = document.getElementById("banditClassButton").onclick = displayClass2;
+var classBtn = document.getElementById("astrologerClassButton").onclick = displayClass3;
+var classBtn = document.getElementById("warriorClassButton").onclick = displayClass4;
+var classBtn = document.getElementById("prisonerClassButton").onclick = displayClass5;
+var classBtn = document.getElementById("confessorClassButton").onclick = displayClass6;
+var classBtn = document.getElementById("wretchClassButton").onclick = displayClass7;
+var classBtn = document.getElementById("vagabondClassButton").onclick = displayClass8;
+var classBtn = document.getElementById("prophetClassButton").onclick = displayClass9;
+var classBtn = document.getElementById("samuraiClassButton").onclick = displayClass10;
+
+function displayClass1()
+{
+    document.getElementById("classDescription").innerHTML = Hero;
+	console.log(Hero);
+}
+
+function displayClass2()
+{
+    document.getElementById("classDescription").innerHTML = Bandit;
+	console.log(Bandit);
+}
+
+function displayClass3()
+{
+    document.getElementById("classDescription").innerHTML = Astrologer;
+	console.log(Astrologer);
+}
+
+function displayClass4()
+{
+    document.getElementById("classDescription").innerHTML = Warrior;
+	console.log(Warrior);
+}
+
+function displayClass5()
+{
+    document.getElementById("classDescription").innerHTML = Prisoner;
+	console.log(Prisoner);
+}
+
+function displayClass6()
+{
+    document.getElementById("classDescription").innerHTML = Confessor;
+	console.log(Confessor);
+}
+
+function displayClass7()
+{
+    document.getElementById("classDescription").innerHTML = Wretch;
+	console.log(Wretch);
+}
+
+function displayClass8()
+{
+    document.getElementById("classDescription").innerHTML = Vagabond;
+	console.log(Vagabond);
+}
+
+function displayClass9()
+{
+    document.getElementById("classDescription").innerHTML = Prophet;
+	console.log(Prophet);
+}
+
+function displayClass10()
+{
+    document.getElementById("classDescription").innerHTML = Samurai;
+	console.log(Samurai);
+}
+
+var endHeading = document.getElementById("End Heading");
+endHeading.style.color = "tan"
